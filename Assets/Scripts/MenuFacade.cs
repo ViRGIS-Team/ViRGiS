f1688bec84191587b2664995ec6637141daa3c11
Paul <runette@gmail.com>
2020-04-12 12:49:58 +0100

    Interaction working with VRTK
    Former-commit-id: 2e28c19c7c286a52f2cb911c2f040c193a46e492

---
+++ Assets/Scripts/MenuFacade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFacade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Selected(int button)
+    {
+        Debug.Log(button);
+    }
+
+    void UnSelected(int button)
+    {
+        Debug.Log(button);
+    }
+}
