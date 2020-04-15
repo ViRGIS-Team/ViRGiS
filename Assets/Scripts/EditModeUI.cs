53b7e4ea0a3aca9f3e7c4619f44885db341cb2c5
Paul <runette@gmail.com>
2020-04-15 18:10:13 +0100

    Edit Session Toggle working
    Former-commit-id: e562a846178a8a4e832dc2116a873d8a17228e98

---
+++ Assets/Scripts/EditModeUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditModeUI : MonoBehaviour
+{ 
+    public Toggle EditOn;
+ 
+    // Start is called before the first frame update
+    void Enabled()
+    {
+        EditOn.isOn = Global.EditSession;
+    }
+
+    public void OnChange(bool thisEvent) 
+    {
+        if(thisEvent)
+        {
+            Global.EditSession = true;
+        } else
+        {
+            if (Global.EditSession)
+            {
+                Global.EditSession = false;
+                EventManager eventManager = Global.Map.GetComponent<EventManager>();
+                eventManager.OnEditsessionEnd.Invoke();
+            }
+        }
+    }
+}
