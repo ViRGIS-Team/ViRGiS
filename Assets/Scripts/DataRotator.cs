a301fa3e8deab74dd3af3e0d62b8aad5bf63c434
Paul <runette@gmail.com>
2020-05-02 22:38:21 +0100

    #87 checkpoint 2
    Former-commit-id: 0e789e4e28f5fd68400289d8e9bf2b0efde6808b

--- Assets/Scripts/DataRotator.cs
+++ Assets/Scripts/DataRotator.cs
@@ -2,53 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataRotator : MonoBehaviour
+namespace ViRGIS
 {
-    public Color color;
-    public Color anticolor;
-    public Vector3 position;
 
-    // Start is called before the first frame update
-    void Start()
+    public class DataRotator : MonoBehaviour
     {
-        
-    }
+        public Color color;
+        public Color anticolor;
+        public Vector3 position;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void SetColor(Color newCol)
-    {
-        if (newCol != color)
+        // Start is called before the first frame update
+        void Start()
         {
-            color = newCol;
-            gameObject.BroadcastMessage("SetColor", newCol);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetColor(Color newCol)
+        {
+            if (newCol != color)
+            {
+                color = newCol;
+                gameObject.BroadcastMessage("SetColor", newCol);
+            }
+        }
+
+        public void Selected(int button)
+        {
+            if (button != 100)
+            {
+                gameObject.BroadcastMessage("Selected", 100, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+
+        public void UnSelected(int button)
+        {
+            if (button != 100)
+            {
+                gameObject.BroadcastMessage("UnSelected", 100, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+
+        public void Translate(MoveArgs args)
+        {
+            if (args.translate != Vector3.zero) gameObject.transform.Translate(args.translate);
+            if (args.rotate != Quaternion.identity) gameObject.transform.rotation = gameObject.transform.rotation * args.rotate;
+            if (args.scale != 0.0f) gameObject.transform.localScale = gameObject.transform.localScale * args.scale;
         }
     }
-
-    public void Selected(int button)
-    {
-        if (button != 100)
-        {
-            gameObject.BroadcastMessage("Selected", 100, SendMessageOptions.DontRequireReceiver);
-        }
-    }
-
-    public void UnSelected(int button)
-    {
-        if (button != 100)
-        {
-            gameObject.BroadcastMessage("UnSelected", 100, SendMessageOptions.DontRequireReceiver);
-        }
-    }
-
-    public void Translate(MoveArgs args)
-    {
-        if (args.translate != Vector3.zero) gameObject.transform.Translate(args.translate);
-        if (args.rotate != Quaternion.identity) gameObject.transform.rotation = gameObject.transform.rotation * args.rotate;
-        if (args.scale != 0.0f) gameObject.transform.localScale = gameObject.transform.localScale * args.scale;
-    } 
 }
---
+++ Assets/Scripts/Geometries/DatapointSphere.cs
@@ -0,0 +1,153 @@
+
+// copyright Runette Software Ltd, 2020. All rights reserved﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ViRGIS
+{
+
+
+    /// <summary>
+    /// Controls an instance of a data pointor handle
+    /// </summary>
+    public class DatapointSphere : MonoBehaviour, IVirgisComponent
+    {
+
+        public Color color; // color of the marker
+        public Color anticolor; // color of the market when selected
+        public string gisId; // ID of this market in the geoJSON
+        public IDictionary<string, object> gisProperties; //  geoJSON properties of this marker
+
+        public int id; // internal ID for this marker - used when it is part of a larger structure
+        private Renderer thisRenderer; // convenience link to the rendere for this marker
+        private Transform label; //  Go of the label or billboard
+
+
+        void Start()
+        {
+            thisRenderer = GetComponent<Renderer>();
+            if (color != null)
+            {
+                thisRenderer.material.SetColor("_BaseColor", color);
+            }
+            if (transform.childCount > 0) label = transform.GetChild(0);
+        }
+
+        /// <summary>
+        /// Every frame - realign the billboard
+        /// </summary>
+        void Update()
+        {
+            if (label) label.LookAt(Global.mainCamera.transform);
+
+        }
+
+        /// <summary>
+        ///  On selected - change color and send message up the entity tree
+        /// </summary>
+        /// <param name="button">SelecetionType Identifies the user action type that led to selection</param>
+        public void Selected(SelectionTypes button)
+        {
+            thisRenderer.material.SetColor("_BaseColor", anticolor);
+            if (button != SelectionTypes.BROADCAST)
+            {
+                gameObject.transform.parent.gameObject.SendMessageUpwards("Selected", button, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+
+        /// <summary>
+        /// On unselected - change color and send message up the entity tree
+        /// </summary>
+        /// <param name="button">SelecetionType Identifies the user action type that led to selection</param>
+        public void UnSelected(SelectionTypes button)
+        {
+            thisRenderer.material.SetColor("_BaseColor", color);
+            if (button != SelectionTypes.BROADCAST)
+            {
+                gameObject.transform.parent.gameObject.SendMessageUpwards("UnSelected", button, SendMessageOptions.DontRequireReceiver);
+            }
+
+        }
+
+        /// <summary>
+        /// Set the color for the marker
+        /// </summary>
+        /// <param name="newColor"> Color</param>
+        public void SetColor(Color newColor)
+        {
+            color = newColor;
+            anticolor = Color.white - newColor;
+            anticolor.a = color.a;
+            Renderer thisRenderer = GetComponent<Renderer>();
+            if (thisRenderer)
+            {
+                thisRenderer.material.SetColor("_BaseColor", color);
+            }
+        }
+
+        /// <summary>
+        /// Sent by the UI to request this marker to move.
+        /// </summary>
+        /// <param name="newPos">Vector3 Worldspace Location to move to </param>
+        public void MoveTo(Vector3 newPos)
+        {
+            MoveArgs args = new MoveArgs();
+            args.translate = newPos - transform.position;
+            args.oldPos = transform.position;
+            args.id = id;
+            args.pos = newPos;
+            SendMessageUpwards("Translate", args, SendMessageOptions.DontRequireReceiver);
+        }
+
+        /// <summary>
+        ///  Sent by the parent entity to request this market to move as part of an entity move
+        /// </summary>
+        /// <param name="argsin">MoveArgs</param>
+        void TranslateHandle(MoveArgs argsin)
+        {
+            if (argsin.id == id && argsin.pos != transform.position)
+            {
+                MoveArgs argsout = new MoveArgs();
+                argsout.oldPos = transform.position;
+                transform.position = argsin.pos;
+                argsout.id = id;
+                argsout.pos = transform.position;
+                SendMessageUpwards("VertexMove", argsout, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+
+        /// <summary>
+        /// Set the Id of the marker
+        /// </summary>
+        /// <param name="value">ID</param>
+        public void SetId(int value)
+        {
+            id = value;
+        }
+
+        /// <summary>
+        /// Callled on an ExitEditSession event
+        /// </summary>
+        public void EditEnd()
+        {
+
+        }
+
+        /// <summary>
+        /// received when a Move Axis request is made by the user
+        /// </summary>
+        /// <param name="delta"> Vector representing this channge to the transform</param>
+        public void MoveAxis(MoveArgs args)
+        {
+            if (args.pos == null)
+            {
+                args.translate = args.pos - transform.position;
+            }
+            else
+            {
+                args.pos = transform.position;
+            }
+            transform.parent.SendMessageUpwards("MoveAxis", args, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
