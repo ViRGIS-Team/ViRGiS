7546af48d4714e5ec3a59930dc28f9780d9ddd07
ipnq <dipnqd@gmail.com>
2020-05-19 19:11:56 +0800

    add features - poc
    Former-commit-id: 56379c99e92d84a86bdbff0bd338d274a7edf5ae

---
+++ Assets/Scripts/ShapeAdder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeAdder : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public GameObject theCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LeftTriggerPressed(bool activate) {
+        print($"LeftTriggerPressed: activate = {activate}");
+        Vector3 pos = theCube.transform.position;
+        Quaternion rot = theCube.transform.rotation;
+        GameObject newCube = Instantiate(cubePrefab, pos, rot);
+    }
+
+    public void LeftTriggerReleased(bool activate) {
+        print($"LeftTriggerReleased: activate = {activate}");
+    }
+
+}
