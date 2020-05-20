d857f58183d9ff00071a060d2d2aca73c75c02db
ipnq <dipnqd@gmail.com>
2020-05-20 18:46:00 +0800

    add features - checkpoint 1
    add Point feature
    
    
    Former-commit-id: 2a9543e459c276f1adcd237184ba01a53d60cf8f

--- Assets/Scripts/Layers/PointLayer.cs
+++ Assets/Scripts/Layers/PointLayer.cs
@@ -1,20 +1,16 @@
 // copyright Runette Software Ltd, 2020. All rights reserved
-using System.Collections.Generic;
-using UnityEngine;
-using GeoJSON.Net.Geometry;
-using GeoJSON.Net.Feature;
 using GeoJSON.Net;
-using System.Threading.Tasks;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
 using Project;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.UI;
-using Zinnia.Extension;
-using System.Reflection.Emit;
 
-namespace Virgis
-{
+namespace Virgis {
 
-    public class PointLayer : Layer<GeographyCollection, FeatureCollection>
-    {
+    public class PointLayer : Layer<GeographyCollection, FeatureCollection> {
         // The prefab for the data points to be instantiated
         public GameObject SpherePrefab;
         public GameObject CubePrefab;
@@ -26,27 +22,44 @@ namespace Virgis
 
         private GameObject PointPrefab;
 
-        protected override async Task _init(GeographyCollection layer)
-        {
+        protected override async Task _init(GeographyCollection layer) {
             geoJsonReader = new GeoJsonReader();
             await geoJsonReader.Load(layer.Source);
             features = geoJsonReader.getFeatureCollection();
         }
 
-        protected override void _addFeature(MoveArgs args)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void _draw()
-        {
+        protected override void _addFeature(MoveArgs args) {
             Dictionary<string, Unit> symbology = layer.Properties.Units;
             float displacement = 1.0f;
-            if (symbology.ContainsKey("point") && symbology["point"].ContainsKey("Shape"))
-            {
+            GameObject dataPoint = Instantiate(PointPrefab, args.pos, args.rotate, transform);
+            Datapoint com = dataPoint.GetComponent<Datapoint>();
+            //com.gisId = gisId;
+            //com.gisProperties = properties;
+            //Set the symbology
+            if (symbology.ContainsKey("point")) {
+                dataPoint.SendMessage("SetColor", (Color) symbology["point"].Color);
+                dataPoint.transform.localScale = symbology["point"].Transform.Scale;
+                //dataPoint.transform.localRotation = symbology["point"].Transform.Rotate;
+                //dataPoint.transform.localPosition = symbology["point"].Transform.Position;
+                //dataPoint.transform.position = position;
+            }
+
+            //Set the label
+            GameObject labelObject = Instantiate(LabelPrefab, dataPoint.transform, false);
+            labelObject.transform.localScale = labelObject.transform.localScale * Vector3.one.magnitude / dataPoint.transform.localScale.magnitude;
+            labelObject.transform.localPosition = Vector3.up * displacement;
+            Text labelText = labelObject.GetComponentInChildren<Text>();
+            labelText.text = "New Feature";
+
+            //            throw new System.NotImplementedException();
+        }
+
+        protected override void _draw() {
+            Dictionary<string, Unit> symbology = layer.Properties.Units;
+            float displacement = 1.0f;
+            if (symbology.ContainsKey("point") && symbology["point"].ContainsKey("Shape")) {
                 Shapes shape = symbology["point"].Shape;
-                switch (shape)
-                {
+                switch (shape) {
                     case Shapes.Spheroid:
                         PointPrefab = SpherePrefab;
                         break;
@@ -61,29 +74,22 @@ namespace Virgis
                         PointPrefab = SpherePrefab;
                         break;
                 }
-            }
-            else
-            {
+            } else {
                 PointPrefab = SpherePrefab;
             }
 
-            foreach (Feature feature in features.Features)
-            {
+            foreach (Feature feature in features.Features) {
                 // Get the geometry
                 MultiPoint mPoint = null;
-                if (feature.Geometry.Type == GeoJSONObjectType.Point)
-                {
+                if (feature.Geometry.Type == GeoJSONObjectType.Point) {
                     mPoint = new MultiPoint(new List<Point>() { feature.Geometry as Point });
-                }
-                else if (feature.Geometry.Type == GeoJSONObjectType.MultiPoint)
-                {
+                } else if (feature.Geometry.Type == GeoJSONObjectType.MultiPoint) {
                     mPoint = feature.Geometry as MultiPoint;
                 }
 
                 Dictionary<string, object> properties = feature.Properties as Dictionary<string, object>;
                 string gisId = feature.Id;
-                foreach (Point geometry in mPoint.Coordinates)
-                {
+                foreach (Point geometry in mPoint.Coordinates) {
                     Position in_position = geometry.Coordinates as Position;
                     Vector3 position = in_position.Vector3();
 
@@ -96,9 +102,8 @@ namespace Virgis
                     com.gisProperties = properties;
 
                     //Set the symbology
-                    if (symbology.ContainsKey("point"))
-                    {
-                        dataPoint.SendMessage("SetColor", (Color)symbology["point"].Color);
+                    if (symbology.ContainsKey("point")) {
+                        dataPoint.SendMessage("SetColor", (Color) symbology["point"].Color);
                         dataPoint.transform.localScale = symbology["point"].Transform.Scale;
                         dataPoint.transform.localRotation = symbology["point"].Transform.Rotate;
                         dataPoint.transform.localPosition = symbology["point"].Transform.Position;
@@ -106,26 +111,24 @@ namespace Virgis
                     }
 
                     //Set the label
-                    GameObject labelObject = Instantiate(LabelPrefab,  dataPoint.transform, false);
+                    GameObject labelObject = Instantiate(LabelPrefab, dataPoint.transform, false);
                     labelObject.transform.localScale = labelObject.transform.localScale * Vector3.one.magnitude / dataPoint.transform.localScale.magnitude;
                     labelObject.transform.localPosition = Vector3.up * displacement;
                     Text labelText = labelObject.GetComponentInChildren<Text>();
 
-                    if (symbology.ContainsKey("point") && symbology["point"].ContainsKey("Label") && symbology["point"].Label != null && properties.ContainsKey(symbology["point"].Label))
-                    {
-                        labelText.text = (string)properties[symbology["point"].Label];
+                    if (symbology.ContainsKey("point") && symbology["point"].ContainsKey("Label") && symbology["point"].Label != null && properties.ContainsKey(symbology["point"].Label)) {
+                        labelText.text = (string) properties[symbology["point"].Label];
                     }
                 }
             };
         }
 
-        protected override void _checkpoint() { }
-        protected override void _save()
-        {
+        protected override void _checkpoint() {
+        }
+        protected override void _save() {
             Datapoint[] pointFuncs = gameObject.GetComponentsInChildren<Datapoint>();
             List<Feature> thisFeatures = new List<Feature>();
-            foreach (Datapoint pointFunc in pointFuncs)
-            {
+            foreach (Datapoint pointFunc in pointFuncs) {
                 thisFeatures.Add(new Feature(pointFunc.gameObject.transform.position.ToPoint(), pointFunc.gisProperties, pointFunc.gisId));
             }
             FeatureCollection FC = new FeatureCollection(thisFeatures);
@@ -135,14 +138,12 @@ namespace Virgis
         }
 
 
-        public override void Translate(MoveArgs args)
-        {
+        public override void Translate(MoveArgs args) {
             gameObject.BroadcastMessage("TranslateHandle", args, SendMessageOptions.DontRequireReceiver);
             changed = true;
         }
 
-        public override void MoveAxis(MoveArgs args)
-        {
+        public override void MoveAxis(MoveArgs args) {
 
         }
     }
--- Assets/Scripts/ShapeAdder.cs
+++ Assets/Scripts/ShapeAdder.cs
@@ -1,33 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ShapeAdder : MonoBehaviour
-{
-    public GameObject cubePrefab;
-    public GameObject theCube;
+namespace Virgis {
+    public class ShapeAdder : MonoBehaviour {
+        public GameObject cubePrefab;
+        public GameObject theCube;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        private AppState _appState;
+
+        // Start is called before the first frame update
+        void Start() {
+            Debug.Log("ShapeAdder awakens");
+            _appState = AppState.instance;
+            _appState.editSession.AddStartEditSessionListener(OnStartEditSession);
+            _appState.editSession.AddEndEditSessionListener(OnEndEditSession);
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public void LeftTriggerPressed(bool activate) {
+            Debug.Log($"LeftTriggerPressed: activate = {activate}");
+            if (_appState.editSession.IsActive()) {
+                ILayer editableLayer = _appState.editableLayer;
+                MoveArgs args = new MoveArgs();
+                args.pos = theCube.transform.position;
+                args.rotate = theCube.transform.rotation;
+                editableLayer.AddFeature(args);
+            }
+            //Vector3 pos = theCube.transform.position;
+            //Quaternion rot = theCube.transform.rotation;
+            //GameObject newCube = Instantiate(cubePrefab, pos, rot);
+        }
+
+        public void LeftTriggerReleased(bool activate) {
+            Debug.Log($"LeftTriggerReleased: activate = {activate}");
+        }
+
+        private void OnStartEditSession() {
+            Debug.Log("ShapeAdder OnStartEditSession");
+            theCube.SetActive(true);
+        }
+
+        private void OnEndEditSession(bool saved) {
+            Debug.Log("ShapeAdder OnEndEditSession");
+            theCube.SetActive(false);
+        }
+
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void LeftTriggerPressed(bool activate) {
-        print($"LeftTriggerPressed: activate = {activate}");
-        Vector3 pos = theCube.transform.position;
-        Quaternion rot = theCube.transform.rotation;
-        GameObject newCube = Instantiate(cubePrefab, pos, rot);
-    }
-
-    public void LeftTriggerReleased(bool activate) {
-        print($"LeftTriggerReleased: activate = {activate}");
-    }
-
 }
