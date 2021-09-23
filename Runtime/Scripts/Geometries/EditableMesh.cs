ef9fe5757950f9f933a9424665edb0880b68d9c8
Paul <runette@gmail.com>
2021-09-23 22:03:28 +0100

    Mesh Edit basically working

--- Runtime/Scripts/Geometries/EditableMesh.cs
+++ Runtime/Scripts/Geometries/EditableMesh.cs
@@ -31,15 +31,15 @@ using System.Linq;
 public class EditableMesh : VirgisFeature
 {   
     private bool m_BlockMove = false; // is entity in a block-move state
-    private DMesh3 m_dmesh; // This is the mesh expressed as a DMesh3 in either local
-                            // space coordinates or projected coordinates depending 
-                            // on m_project 
+    private DMesh3 m_dmesh; // This is the mesh expressed as a DMesh3 in local
+                            // space coordinates 
+    private DMesh3 m_oldDmesh; // holds the previous mesh during editing
     private int m_selectedVertex;
     private GameObject m_sphere;
 
     private Vector3 m_currentHit; // current hit vertex
     private int? m_currentHitTri; // current hit triangle
-    private DMesh3 m_newDmesh; // used when editing the mesh to hold the new reult
+    private DMeshAABBTree3 m_aabb; // AABB Tree for current m,esh
     private int n = 0;
     private List<Int32> m_nRing;
     private bool m_selectOn = false;
@@ -49,7 +49,7 @@ public class EditableMesh : VirgisFeature
         if (m_selectOn)
             UnSelected(SelectionType.SELECT);
         m_selectOn = true;
-        m_newDmesh = m_dmesh;
+        m_oldDmesh = new DMesh3(m_dmesh);
         transform.parent.SendMessage("Selected", button, SendMessageOptions.DontRequireReceiver);
         if (button == SelectionType.SELECTALL) {
             m_BlockMove = true;
@@ -58,140 +58,21 @@ public class EditableMesh : VirgisFeature
             Mesh mesh = mf.sharedMesh;
             m_currentHitTri = null;
             m_currentHit = transform.InverseTransformPoint(AppState.instance.lastHitPosition);
-            Vector3d target = m_currentHit;
-            System.Random rand = new System.Random();
-            int count = 0;
-            //
-            // The algorithm will find local optima - repeat until you get the tru optima
-            // but limit the interation using a count
-            //
-            Int32 current = 0;
-            while (count < m_dmesh.VertexCount) {
-                count ++;
-                //
-                // choose a random starting point
-                //
-                current = rand.Next(0, m_dmesh.VertexCount);
-                Vector3d vtx = m_dmesh.GetVertex(current);
-                double currentDist = vtx.DistanceSquared(target);
-                //
-                // find the ring of triangles around the current point
-                //
-                int iter = 0;
-                while (true) {
-                    iter++;
-                    //    throw new InvalidOperationException("Meh One Ring operation invalid : " + res.ToString());
-                    //
-                    // Interate through the vertices in the one Ring and find the clost to the target point
-                    //
-                    bool flag = false;
-                    foreach (Int32 v in m_dmesh.VtxVerticesItr(current)) {
-                        Vector3d thisVtx = m_dmesh.GetVertex(v);
-                        double thisDist = thisVtx.DistanceSquared(target);
-                        if (thisDist < currentDist) {
-                            flag = true;
-                            current = v;
-                            vtx = thisVtx;
-                            currentDist = thisDist;
-                        }
-                    }
-                    //
-                    // if the current point as closest - then  have a local optima
-                    //
-                    if (!flag)
-                        break;
-                }
-                //
-                // we now have a local optima
-                // to check for a global optima look to see if hit is contained by one of the triangles in the one ring
-                //
-                bool f2 = false;
-                foreach (Int32 t in m_dmesh.VtxTrianglesItr(current)) {
-                    Index3i tri = m_dmesh.GetTriangle(t);
-                    Triangle3d triangle = new Triangle3d(
-                            m_dmesh.GetVertex(tri.a),
-                            m_dmesh.GetVertex(tri.b),
-                            m_dmesh.GetVertex(tri.c)
-                        );
-                    double[] xs = new double[3] { triangle.V0.x, triangle.V1.x, triangle.V2.x };
-                    double[] ys = new double[3] { triangle.V0.y, triangle.V1.y, triangle.V2.y };
-                    double[] zs = new double[3] { triangle.V0.z, triangle.V1.z, triangle.V2.z };
-
-                    if (
-                        target.x >= xs.Min() &&
-                        target.x <= xs.Max() &&
-                        target.y >= ys.Min() &&
-                        target.y <= ys.Max() &&
-                        target.z >= zs.Min() &&
-                        target.z <= zs.Max()
-                        ) {
-                        f2 = true;
-                        m_currentHitTri = t;
-                    }
-                }
-                //
-                // if we found on triangle that contain the current hit then we have finished
-                //
-                if (f2)
-                    break;
-            }
-            if (count >= m_dmesh.VertexCount) {
-                //
-                // This is the unoptimized verion but it is guaranteed to find a solution if one exits
-                //
-
-                current = -1;
-                float currentDist = float.MaxValue;
-                if (m_currentHit != null) {
-                    for (int i = 0; i < mesh.vertices.Length; i++) {
-                        Vector3 vtx = mesh.vertices[i];
-                        float dist = (m_currentHit - vtx).sqrMagnitude;
-                        if (dist < currentDist) {
-                            current = i;
-                            currentDist = dist;
-                        }
-                    }
-                }
-                //
-                // Check that the closest vertex to the point is an actual solution
-                //
-                bool f2 = false;
-                foreach (Int32 t in m_dmesh.VtxTrianglesItr(current)) {
-                    Index3i tri = m_dmesh.GetTriangle(t);
-                    Triangle3d triangle = new Triangle3d(
-                            m_dmesh.GetVertex(tri.a),
-                            m_dmesh.GetVertex(tri.b),
-                            m_dmesh.GetVertex(tri.c)
-                        );
-                    double[] xs = new double[3] { triangle.V0.x, triangle.V1.x, triangle.V2.x };
-                    double[] ys = new double[3] { triangle.V0.y, triangle.V1.y, triangle.V2.y };
-                    double[] zs = new double[3] { triangle.V0.z, triangle.V1.z, triangle.V2.z };
-
-                    // Use an AABB appraoch to check hit
-                    if (
-                        target.x >= xs.Min() &&
-                        target.x <= xs.Max() &&
-                        target.y >= ys.Min() &&
-                        target.y <= ys.Max() &&
-                        target.z >= zs.Min() &&
-                        target.z <= zs.Max()
-                        ) {
-                        f2 = true;
-                        m_currentHitTri = t;
-                    }
-                }
-                //
-                // if we found one triangle that contain the current hit then we have finished
-                //
-                if (!f2) {
-                    Debug.LogError(" Mesh Vertex Search : No Solution Found");
-                    m_selectOn = false;
-                    return;
-                }
-            }
-            m_selectedVertex = current;
+            m_currentHitTri = m_aabb.FindNearestTriangle(m_currentHit);
+            Vector3d V0 = new Vector3d();
+            Vector3d V1 = new Vector3d();
+            Vector3d V2 = new Vector3d();
+            m_dmesh.GetTriVertices(m_currentHitTri.Value, ref V0, ref V1, ref V2);
+            Index3i tri = m_dmesh.GetTriangle(m_currentHitTri.Value);
+            Vector3d currentBari = MathUtil.BarycentricCoords(m_currentHit, V0, V1, V2);
+            if (currentBari.x > currentBari.y && currentBari.x > currentBari.z)
+                m_selectedVertex = tri.a;
+            if (currentBari.y > currentBari.x && currentBari.y > currentBari.z)
+                m_selectedVertex = tri.b;
+            if (currentBari.z > currentBari.y && currentBari.z > currentBari.x)
+                m_selectedVertex = tri.c;
             m_sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            m_sphere.transform.position = transform.TransformPoint(mesh.vertices[current]);
+            m_sphere.transform.position = transform.TransformPoint(mesh.vertices[m_selectedVertex]);
             m_sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
             m_sphere.transform.parent = transform;
         }
@@ -208,7 +89,7 @@ public class EditableMesh : VirgisFeature
         Mesh mesh = mf.sharedMesh;
         mc[0].sharedMesh = mesh;
         mc[1].sharedMesh = ReverseMesh(mesh);
-        m_dmesh = m_newDmesh;
+        m_aabb = new DMeshAABBTree3(m_dmesh, true);
         n = -1;
     }
 
@@ -258,18 +139,17 @@ public class EditableMesh : VirgisFeature
                         deform.SetConstraint(v, m_dmesh.GetVertex(v), 10, false);
                     }
                     deform.SolveAndUpdateMesh();
-                    m_newDmesh = deform.Mesh;
                 } else {
                     m_dmesh.SetVertex(m_selectedVertex, target);
                 }
                 //
-                // reset the Unity meh
+                // reset the Unity mesh
                 // 
                 if (m_sphere != null)
                     m_sphere.transform.localPosition = (Vector3) m_dmesh.GetVertex(m_selectedVertex);
                 List<Vector3> vtxs = new List<Vector3>();
-                foreach (int v in m_newDmesh.VertexIndices())
-                    vtxs.Add((Vector3)m_newDmesh.GetVertex(v));
+                foreach (int v in m_dmesh.VertexIndices())
+                    vtxs.Add((Vector3)m_dmesh.GetVertex(v));
                 mesh.vertices = vtxs.ToArray();
                 mesh.RecalculateBounds();
                 mesh.RecalculateNormals();
@@ -311,6 +191,7 @@ public class EditableMesh : VirgisFeature
         mainMat = mat;
         selectedMat = Wf;
         m_dmesh = dmeshin.Compactify();
+        m_aabb = new DMeshAABBTree3(m_dmesh, true);
         MeshFilter mf = GetComponent<MeshFilter>();
         MeshCollider[] mc = GetComponents<MeshCollider>();
         mr = GetComponent<MeshRenderer>();
@@ -367,29 +248,39 @@ public class EditableMesh : VirgisFeature
     }
 
     public override VirgisFeature AddVertex(Vector3 position) {
-        if (m_currentHit != null && m_currentHitTri != null) {
-            Vector3d V0 = new Vector3d();
-            Vector3d V1 = new Vector3d();
-            Vector3d V2 = new Vector3d();
-            m_newDmesh.GetTriVertices(m_currentHitTri.Value, ref V0, ref V1, ref V2);
-            Index3i tri = m_newDmesh.GetTriangle(m_currentHitTri.Value);
-            Vector3d bari = MathUtil.BarycentricCoords(m_currentHit, V0, V1, V2);
-            int edgeId = 0;
-            if (bari.x > bari.y && bari.x > bari.z)
-                edgeId = m_newDmesh.FindEdgeFromTri(tri.b, tri.c, m_currentHitTri.Value);
-            if (bari.y > bari.x && bari.y > bari.z)
-                edgeId = m_newDmesh.FindEdgeFromTri(tri.a, tri.c, m_currentHitTri.Value);
-            if (bari.z > bari.y && bari.z > bari.x)
-                edgeId = m_newDmesh.FindEdgeFromTri(tri.b, tri.a, m_currentHitTri.Value);
-            DMesh3.EdgeSplitInfo result = new DMesh3.EdgeSplitInfo();
-            m_newDmesh.SplitEdge(edgeId, out result);
-            Mesh tempMesh = m_newDmesh.ToMesh();
-            tempMesh.RecalculateBounds();
-            tempMesh.RecalculateNormals();
-            MeshFilter mf = GetComponent<MeshFilter>();
-            mf.mesh = tempMesh;
-            UnSelected(SelectionType.SELECT);
-        }
+        Vector3d localPosition = (Vector3d) transform.InverseTransformPoint(position);
+        int currentHitTri = m_aabb.FindNearestTriangle(localPosition);
+        Vector3d V0 = new Vector3d();
+        Vector3d V1 = new Vector3d();
+        Vector3d V2 = new Vector3d();
+        m_dmesh.GetTriVertices(currentHitTri, ref V0, ref V1, ref V2);
+        Index3i tri = m_dmesh.GetTriangle(currentHitTri);
+        Vector3d currentBari = MathUtil.BarycentricCoords(localPosition, V0, V1, V2);
+        int edgeId = 0;
+        if (currentBari.x > currentBari.y && currentBari.x > currentBari.z) 
+            if (currentBari.y < currentBari.z) 
+                edgeId = m_dmesh.FindEdgeFromTri(tri.a, tri.c, currentHitTri);
+            else
+                edgeId = m_dmesh.FindEdgeFromTri(tri.a, tri.b, currentHitTri);
+        if (currentBari.y > currentBari.x && currentBari.y > currentBari.z)
+            if (currentBari.x < currentBari.z)
+                edgeId = m_dmesh.FindEdgeFromTri(tri.b, tri.c, currentHitTri);
+            else
+                edgeId = m_dmesh.FindEdgeFromTri(tri.b, tri.a, currentHitTri);
+        if (currentBari.z > currentBari.y && currentBari.z > currentBari.x)
+            if (currentBari.y < currentBari.x)
+                edgeId = m_dmesh.FindEdgeFromTri(tri.c, tri.a, currentHitTri);
+            else
+                edgeId = m_dmesh.FindEdgeFromTri(tri.c, tri.b, currentHitTri);
+        DMesh3.EdgeSplitInfo result = new DMesh3.EdgeSplitInfo();
+        m_dmesh.SplitEdge(edgeId, out result);
+        m_dmesh.SetVertex(result.vNew, localPosition);
+        Mesh tempMesh = m_dmesh.ToMesh();
+        tempMesh.RecalculateBounds();
+        tempMesh.RecalculateNormals();
+        MeshFilter mf = GetComponent<MeshFilter>();
+        mf.mesh = tempMesh;
+        UnSelected(SelectionType.SELECT);
         return this;
     }
 
@@ -398,15 +289,15 @@ public class EditableMesh : VirgisFeature
     /// </summary>
     public void Delete() {
         MeshFilter mf = GetComponent<MeshFilter>();
-        m_newDmesh.RemoveVertex(m_selectedVertex, true, true);
-        if (m_dmesh.IsClosed()) {
-            MeshAutoRepair mr = new MeshAutoRepair(m_newDmesh);
+        m_dmesh.RemoveVertex(m_selectedVertex);
+        if (m_oldDmesh.IsClosed()) {
+            MeshAutoRepair mr = new MeshAutoRepair(m_dmesh);
             mr.Apply();
-            m_newDmesh = mr.Mesh;
+            m_dmesh = mr.Mesh;
         } else {
-            m_newDmesh.CompactInPlace();
+            m_dmesh.CompactInPlace();
         }
-        Mesh tempMesh = m_newDmesh.ToMesh();
+        Mesh tempMesh = m_dmesh.ToMesh();
         tempMesh.RecalculateBounds();
         tempMesh.RecalculateNormals();
         mf.mesh = tempMesh;
